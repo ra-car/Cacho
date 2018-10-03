bb31bdb78775defdc40476f60f2d35cca0d3d0d9
Rafael <r.cardenas11@uniandes.edu.co>
2018-10-03 15:21:35 -0500

    Login Y registro

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,16 +3,22 @@ import PropTypes from "prop-types";
 import {Meteor} from "meteor/meteor";
 import {withTracker} from "meteor/react-meteor-data";
 import {Usuarios} from "../api/usuarios.js"; 
+import AccountsUIWrapper from "./AccountsUIWrapper.js";
 
 class App extends Component {
   render() {
     return (
       <div>        
-        <nav className ="navbar navbar-dark bg-dark">
+        <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
          <a className ="navbar-brand" href="#">
           <img src="/images/dice.png" width="30" height="30" alt="dados"/>
           Cacho
          </a>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-item nav-link" href="#"><AccountsUIWrapper/></a>
+              </div>
+          </div>
         </nav>
         <ul>
           {this.props.usuarios.map( (u,i) =>
